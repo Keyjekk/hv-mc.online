fecc566f4ea5e3b92eb585f9a8b3ae40837bf067
Keyjekk <108344123+Keyjekk@users.noreply.github.com>
2024-08-02 23:06:19 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-            // Проверяем, что Telegram Web App API доступен
-            if (window.Telegram.WebApp) {
-                // Разворачиваем приложение на весь экран
-                Telegram.WebApp.expand();
-}
-});
